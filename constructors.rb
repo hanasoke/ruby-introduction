04ebf07ae7f0fed1626ada477db4a0f490a8b757
hanasoke@gmail.com <hanasoke@gmail.com>
2024-12-21 11:23:54 +0700

    Adding a constructor

---
+++ constructors.rb
@@ -0,0 +1,17 @@
+# A Ruby program to demonstrate 
+# the working of constructor
+
+#!/usr/bin/ruby
+
+# class name
+class Demo 
+
+    # constructor 
+    def initialize
+        puts "Welcome to Malaysia"
+    end 
+
+end 
+
+# Creating Object 
+Demo.new 
