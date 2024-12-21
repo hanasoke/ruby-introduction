5ca720907019557751387a88ca6099ed3bd876d6
hanasoke@gmail.com <hanasoke@gmail.com>
2024-12-21 11:08:03 +0700

    learning class_object

---
+++ class_object.rb
@@ -0,0 +1,93 @@
+# Ruby program to illustrate
+# the defining of methods 
+
+# !/usr/bin/ruby
+
+# defining class Vehicle 
+# class GFG
+    # defining method 
+    # def geeks
+    # printing result 
+    # puts "Hanas Bayu Pratama, B.ICT"
+    # end of method 
+    # end
+# end of class GFG
+# end
+
+# creating object
+# obj = GFG.new
+
+# calling method using object
+# obj.geeks
+
+# Defining class Car
+# class Car 
+# defining method 
+    # def brand 
+    # printing result
+    # puts "BWD"
+    # puts "Mitsubishi"
+    # puts "Honda"
+    # puts "Daihatsu"
+    # puts "Tesla"
+    # end of method
+    # end
+# end of class GFG
+# end
+
+# creating object 
+# car = Car.new 
+# calling method using object
+# car.brand
+
+# defining class Vehicle
+# class Vehicle 
+    # initialize method 
+#     def initialize(id, color, name)
+#         # variables 
+#         @veh_id = id 
+#         @veh_color = color 
+#         @veh_name = name 
+#         # displaying values 
+#         puts "ID is: #@veh_id"
+#         puts "Color is: #@veh_color"
+#         puts "Name is: #@veh_name"
+#         puts "\n"
+#     end 
+# end 
+
+# Creating objects and passing parameters
+# to new method
+# xveh = Vehicle. new("1", "Red", "ABC")
+# yveh = Vehicle. new("2", "Black", "XYZ")
+
+# defining class Car
+# class Car 
+    # initialize method
+#     def initialize(id, name, type, brand, manufacture, price)
+#          # variables
+#         @car_id = id 
+#         @car_name = name
+#         @car_type = type 
+#         @car_brand = brand 
+#         @car_manufacture = manufacture
+#         @car_price = price 
+#         # displaying values 
+#         puts "ID : #@car_id"
+#         puts "Name : #@car_name"
+#         puts "Type : #@car_type"
+#         puts "Brand : #@car_brand"
+#         puts "Manufacture : #@car_manufacture"
+#         puts "price : #@car_price"
+#         puts "\n"
+#     end 
+# end 
+
+# Creating objects and passing parameters
+# to new method 
+
+# car1 = Car. new("1", "BYD M6", "Matic", "BYD", 2020, 467000000)
+# car2 = Car. new("2", "Avanza G", "Manual", "Toyota", 2002, 120000000)
+# car3 = Car. new("3", "BWD Seal", "Matic", "BWD", 2022, 45000000)
+# car4 = Car. new("4", "BWD Atto 3", "Matic", "BWD", 2023, 67000000)
+# car5 = Car. new("5", "BWD Dolphin", "Matic", "BWD", 2024, 79000000)
